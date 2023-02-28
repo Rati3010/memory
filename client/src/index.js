4a37c2381251cbdba01283088433eed4af879b44
Rati3010 <mratikanta1997@gmail.com>
2023-03-01 05:09:52 +0530

    added

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
-import ReactDOM  from "react-dom";
-
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import App from "./App";
+import store from "./Redux/store";
 
-// const root = ReactDOM.createRoot(document.getElementById("root"))
-// root.render(<App/>)
-
-ReactDOM.render(<App/>,document.getElementById('root'))
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'))
